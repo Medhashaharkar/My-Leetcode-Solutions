dd89e455583d666523bb6a5a477352d8fe7950b5
Medhashaharkar <medha.shaharkar@gmail.com>
2025-11-01 12:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (10.40%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        my_dict = {}
+
+        for i in range(len(nums)):
+            if target-nums[i] in my_dict.keys():
+                return [i, my_dict[target-nums[i]]]
+            else:
+                my_dict[nums[i]] = i
+                
+
+
+        
